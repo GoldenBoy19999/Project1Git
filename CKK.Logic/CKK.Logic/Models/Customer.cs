d2a4613010bf2ecf6d5ac139a3e625b5d7482f7d
Vic Lara <laravictor19@gmail.com>
2023-11-05 20:30:09 -0600

    Made 4 new classes each following the UML that was given.

---
+++ CKK.Logic/CKK.Logic/Models/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+   public class Customer
+   {
+      private int _id;
+      private string _name;
+      private string _address;
+
+      public int GetId()
+      {
+         return _id;
+      }
+      public void SetId(int Id)
+      {
+         _id = Id;
+      }
+      public string GetName()
+      {
+         return _name;
+      }
+      public void SetName(string Name)
+      {
+         _name = Name;
+      }
+      public string GetAddress()
+      {
+         return _address;
+      }
+      public void SetAddress(string Address)
+      {
+         _address = Address;
+      }
+   }
+}
---
+++ CKK.Logic/CKK.Logic/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+   public class Product
+   {
+      private int _id;
+      private string _name;
+      private decimal _price;
+
+      public int GetId()
+      {
+         return _id;
+      }
+      public void SetId(int Id) 
+      {
+         _id = Id;
+      }
+      public string GetName()
+      {
+         return _name;
+      }
+      public void setName(string Name)
+      {
+         _name = Name;
+      }
+      public decimal GetPrice()
+      {
+         return _price;
+      }
+      public void SetPrice(decimal Price)
+      {
+         _price = Price;
+      }
+   }
+}
