01574e55b01460c821251bf7c5ca9d32391c323c
Vic Lara <laravictor19@gmail.com>
2023-11-15 23:04:52 -0600

    Added storeItem GetProduct

--- CKK.Logic/CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/CKK.Logic/Models/ShoppingCart.cs
@@ -12,5 +12,32 @@ namespace CKK.Logic.Models
       private ShoppingCartItem _product1;
       private ShoppingCartItem _product2;
       private ShoppingCartItem _product3;
+      public ShoppingCart(Customer cust)
+      {
+         _customer = cust;
+      }
+      public int GetCustomerId()
+      {
+         return _customer.GetId();
+      }
+      public StoreItem GetProductById(int id)
+      {
+         if (_product1 != null && _product1.GetProduct().GetId() == id)
+         {
+            return _product1;
+         }
+
+         if (_product2 != null && _product2.GetProduct().GetId() == id)
+         {
+            return _product2;
+         }
+
+         if (_product3 != null && _product3.GetProduct().GetId() == id)
+         {
+            return _product3;
+         }
+
+         return null;
+      }
    }
 }
