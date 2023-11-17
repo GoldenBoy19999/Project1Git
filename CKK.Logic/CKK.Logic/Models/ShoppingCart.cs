8562c5f269060c39155782f0256161753ef1c303
4400092857 <4400092857@BT144-PROG09>
2023-11-16 18:06:32 -0700

    updated and started from new Shoppingcart

--- CKK.Logic/CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/CKK.Logic/Models/ShoppingCart.cs
@@ -18,157 +18,57 @@ namespace CKK.Logic.Models
          _customer = cust;
       }
       public int GetCustomerId()
-      {
-         return _customer.GetId();
-      }
-      public StoreItem GetProductById(int id)
-      {
-         if (_product1 != null && _product1.GetProduct().GetId() == id)
-         {
-            return _product1;
-         }
-
-         if (_product2 != null && _product2.GetProduct().GetId() == id)
-         {
-            return _product2;
-         }
-
-         if (_product3 != null && _product3.GetProduct().GetId() == id)
-         {
-            return _product3;
-         }
-
-         return null;
-      }
-
-      public StoreItem AddProduct(Product prod, int quantity)
-      {
-         if (quantity <= 0)
-         {
-            Console.WriteLine("Quantity must be greater than 0.");
+        {
+            return _customer;
+        }
+        public ShoppingCartItem GetProductById(int id)
+        {
+            if (_product1 != null && _product1.GetProduct().GetId() == id)
+            {
+                return _product1;
+            }
+            if (_product2 != null && _product2.GetProduct().GetId() == id)
+            {
+                return _product2;
+            }
+            if (_product3 != null && _product3.GetProduct().GetId() == id)
+            {
+                return (_product3);
+            }
             return null;
-         }
-
-         var existingProduct = FindProduct(prod);
-
-         if (existingProduct != null)
-         {
-            
-            existingProduct.SetQuantity(existingProduct.GetQuantity() + quantity);
-            return existingProduct;
-         }      
-         return AddNewProduct(prod, quantity);
-      }
-      public StoreItem RemoveProduct(Product prod, int quantity)
-      {
-         if (quantity <= 0)
-         {
-            Console.WriteLine("Quantity must be greater than 0.");
-            return null;
-         }
-         var existingProduct = FindProduct(prod);
-
-         if (existingProduct != null)
-         {
-            int currentQuantity = existingProduct.GetQuantity();
-
-            if (quantity < currentQuantity)
+        }
+        public ShoppingCartItem AddProduct(Product prod)
+        {
+            // runs other add product method with quantity 1
+        }
+        public ShoppingCartItem AddProduct(Product prod, int quantity)
+        {
+            if (quantity <= 0)
             {
-               existingProduct.SetQuantity(currentQuantity - quantity);
-               return existingProduct;
+                Console.WriteLine("Invalid quantity");
+                return null;
             }
-            else if (quantity == currentQuantity)
+            // checks for product and removes quanity if found
+           // returns the product changed or null
+        }
+        public ShoppingCartItem RemoveProduct(Product prod, int quantity)
+        {
+            if (quantity <= 0)
             {
-               RemoveProduct(existingProduct);
-               return existingProduct;
+                Console.WriteLine("Invalid quantity");
+                return null;
             }
-            else
-            {
-               Console.WriteLine("Cant remove more than the current quantity.");
-               return null;
-            }
-         }
-
-         Console.WriteLine("Product not found.");
-         return null;
-      }
-
-      public decimal GetTotal()
-      {
-         decimal total = 0;
-
-         if (_product1 != null)
-         {
-            total += _product1.GetTotal();
-         }
-
-         if (_product2 != null)
-         {
-            total += _product2.GetTotal();
-         }
-
-         if (_product3 != null)
-         {
-            total += _product3.GetTotal();
-         }
-
-         return total;
-      }
-      public StoreItem GetProduct(int prodNum)
-      {
-         if (prodNum == 1)
-         {
-            return _product1;
-         }
-
-         if (prodNum == 2)
-         {
-            return _product2;
-         }
-
-         if (prodNum == 3)
-         {
-            return _product3;
-         }
-
-         Console.WriteLine("Invalid product number.");
-         return null;
-      }
-      public StoreItem FindProduct(Product prod)
-      {
-         if (_product1 != null && _product1.GetProduct().GetId() == prod.GetId())
-         {
-            return _product1;
-         }
-
-         if (_product2 != null && _product2.GetProduct().GetId() == prod.GetId())
-         {
-            return _product2;
-         }
-
-         if (_product3 != null && _product3.GetProduct().GetId() == prod.GetId())
-         {
-            return _product3;
-         }
-
-         return null;
-      }
-
-      public void RemoveProduct(StoreItem product)
-      {
-         if (_product1 == product)
-         {
-            _product1 = null;
-         }
-         else if (_product2 == product)
-         {
-            _product2 = null;
-         }
-         else if (_product3 == product)
-         {
-            _product3 = null;
-         }
-      }
+            // checks for product and removes quantity if found
+            // returns the product changed or null
+        }
+        public decimal GetTotal()
+        {
+            //returns total of all products
+        }
+        public ShoppingCartItem GetProduct(int prodNum)
+        {
+            //returns the product in the position of prodNumor null
+        }
 
    }
 }
